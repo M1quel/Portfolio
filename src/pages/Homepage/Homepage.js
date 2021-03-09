8cdd1ecc67ae1633ad60e5a2b242015a5ae82f43
Mikkel Frederiksen <5760459@rts-365.dk>
2021-03-09 09:27:56 +0100

    homepage main done

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -1,7 +1,26 @@
+import HomepageMain from "../../components/Homepage/HomepageMain/HomepageMain";
 import SecondaryMenu from "../../components/Homepage/SecondaryMenu/SecondaryMenu";
 
 export default function Homepage() {
     return (
+        <>
         <SecondaryMenu/>
+        <HomepageMain
+        heading="hej med dig"
+        img="https://via.placeholder.com/500.png"
+        />
+        <HomepageMain
+        heading="hej med dig"
+        img="https://via.placeholder.com/500.png"
+        />
+        <HomepageMain
+        heading="hej med dig"
+        img="https://via.placeholder.com/500.png"
+        />
+        <HomepageMain
+        heading="hej med dig"
+        img="https://via.placeholder.com/500.png"
+        />
+        </>
     )
 };
